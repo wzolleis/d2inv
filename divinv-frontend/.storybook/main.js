8631621f94db7b4243f14c47faa933f76087aac0
Jack <wzolleis@web.de>
2021-02-17 00:00:25 +0100

    Vereinfachungen für Storybook, verwendet immer noch SCSS :D

--- divinv-frontend/.storybook/main.js
+++ divinv-frontend/.storybook/main.js
@@ -1,23 +1,13 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 module.exports = {
     "stories": [
         "../src/**/*.stories.mdx",
         "../src/**/*.stories.@(js|jsx|ts|tsx)"
     ],
     "addons": [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
+        "@storybook/preset-scss",
         "@storybook/preset-create-react-app",
         "@storybook/preset-typescript",
-        {
-            name: '@storybook/preset-scss',
-            options: {
-                cssLoaderOptions: {
-                    modules: true,
-                    localIdentName: '[name]__[local]--[hash:base64:5]',
-                }
-            }
-        }
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
     ],
 }
--- divinv-frontend/.storybook/preview.js
+++ divinv-frontend/.storybook/preview.js
@@ -1,5 +1,4 @@
-//import '!style-loader!css-loader!sass-loader!../src/scss/index.scss'
-require('!style-loader!css-loader!sass-loader!../src/scss/index.scss');
+import '!style-loader!css-loader!sass-loader!../src/scss/index.scss'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
